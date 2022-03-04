290311885b0677271657440e72c4e0fcee6986f0
amanda-santos <amandasf0200@gmail.com>
2022-03-03 22:30:48 -0300

    ✨ Integrate with FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -15,4 +19,31 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          // condition
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(query.Index("user_by_email"), query.Casefold(email))
+              )
+            ),
+            // then
+            query.Create(query.Collection("users"), { data: { email } }),
+            // else
+            query.Get(
+              query.Match(query.Index("user_by_email"), query.Casefold(email))
+            )
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });

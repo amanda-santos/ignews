99c1ed555b61330740123bbba9d25917b77cd607
amanda-santos <amandasf0200@gmail.com>
2022-03-07 22:37:03 -0300

    ✨ Add subscription with Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from "next-auth/react";
+
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -5,8 +9,32 @@ type SubscribeButtonProps = {
 };
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <button type="button" className={styles["subscribe-button"]}>
+    <button
+      type="button"
+      className={styles["subscribe-button"]}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );

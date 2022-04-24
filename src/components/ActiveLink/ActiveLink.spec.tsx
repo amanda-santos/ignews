539aead4ff0358459bedb5485270ba557b811241
amanda-santos <amandasf0200@gmail.com>
2022-04-24 17:50:55 -0300

    🧪 Test ActiveLink

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,34 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+describe("<ActiveLink />", () => {
+  it("should render correctly", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText("Home")).toBeVisible();
+  });
+
+  it("should receive active class if it is current page", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText("Home")).toHaveClass("active");
+  });
+});

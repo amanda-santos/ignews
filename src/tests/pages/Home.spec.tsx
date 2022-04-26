64c18ca3f10838bdd7b787e6484eae78ca80b23c
amanda-santos <amandasf0200@gmail.com>
2022-04-26 11:08:02 -0300

    🧪 Add tests for Home page

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import Home, { getStaticProps } from "../../pages";
+import { stripe } from "../../services/stripe";
+
+jest.mock("next/router");
+jest.mock("next-auth/react", () => {
+  return {
+    useSession: () => [null, false],
+  };
+});
+jest.mock("../../services/stripe");
+
+describe("Home page", () => {
+  it("should render correctly", () => {
+    render(<Home product={{ priceId: "fake-price-id", amount: "R$ 10,00" }} />);
+    expect(screen.getByText("for R$ 10,00 month.")).toBeVisible();
+  });
+
+  it("loads initial data", async () => {
+    const retrieveStripePricesMock = jest.mocked(stripe.prices.retrieve);
+
+    retrieveStripePricesMock.mockResolvedValueOnce({
+      id: "fake-price-id",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-price-id",
+            amount: "$10.00",
+          },
+        },
+      })
+    );
+  });
+});
